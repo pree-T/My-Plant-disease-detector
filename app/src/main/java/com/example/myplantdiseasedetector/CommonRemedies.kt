565f5e251ab175876951b33cabe71262bc4c4cd9
pree-T <47977131+pree-T@users.noreply.github.com>
2022-02-17 12:22:51 +0530

    First commit

---
+++ app/src/main/java/com/example/myplantdiseasedetector/CommonRemedies.kt
@@ -0,0 +1,11 @@
+package com.example.myplantdiseasedetector
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CommonRemedies : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_common_remedies)
+    }
+}
